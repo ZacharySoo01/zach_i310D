e78c271c90c5474066b93dec796ebfb06b29e2dd
unknown <zacharysoo01@gmail.com>
2023-02-01 15:20:29 -0600

    test1

---
+++ volume_of_sphere.py
@@ -0,0 +1,12 @@
+def calculate_volume_of_sphere(radius):
+    pi = 3.14
+    volume = (4 / 3) * pi * radius **3
+    return volume
+
+radius1 = 30
+vol1 = calculate_volume_of_sphere(radius1)
+print(f"The volume of sphere with radius {radius1} is: {vol1}")
+
+radius2 = 40
+vol2 = calculate_volume_of_sphere(radius2)
+print(f"The volume of sphere with radius {radius2} is: {vol2}")
